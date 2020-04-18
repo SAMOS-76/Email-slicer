7c82fd48d42b6acfb1deb59e28ba00321a53fbd2
Samuel Amos-Osebeyo <sosebeyo@gmail.com>
2020-04-18 18:40:02 +0100

    Added fail safe if email was invalid

--- EmailSlicer_tk.py
+++ EmailSlicer_tk.py
@@ -2,7 +2,6 @@ from tkinter import *
 
 root = Tk()
 
-
 label1 = Label(root, text="Enter Email:").grid(row=0, column=0)
 input_field1 = Entry(root, width=50)
 input_field1.grid(row=0, column=1)
@@ -10,10 +9,13 @@ input_field1.grid(row=0, column=1)
 
 def click():
     email = input_field1.get()
-    username = email.split("@", 1)[0]
-    domain_name = email.split("@", 1)[1]
-    username_label = Label(root, text="Username: " + username).grid(row=2, column=0)
-    domain_name_label = Label(root, text="Domain Name: " + domain_name).grid(row=2, column=1)
+    if email.find("@") == -1:
+        error = Label(root, text="Insert a valid email!").grid(row=2, column=1)
+    else:
+        username = email.split("@", 1)[0]
+        domain_name = email.split("@", 1)[1]
+        username_label = Label(root, text="Username: " + username).grid(row=2, column=0)
+        domain_name_label = Label(root, text="Domain Name: " + domain_name).grid(row=2, column=1)
 
 
 button = Button(root, text="Enter", command=click).grid(row=1, column=1)
